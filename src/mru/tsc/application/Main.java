1f5623dedb147a59e00dd71356862bec0743aba0
Munima <Munima@10.0.0.141>
2021-03-15 13:51:47 -0600

    package updated

--- src/mru/tsc/application/Main.java
+++ src/mru/tsc/application/Main.java
@@ -1,7 +1,9 @@
 package mru.tsc.application;
 	
 import javafx.application.Application;
+
 import javafx.stage.Stage;
+import mru.tsc.view.myGUI;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -9,15 +11,7 @@ import javafx.scene.layout.BorderPane;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		myGUI gui=new myGUI(primaryStage);
 	}
 	
 	public static void main(String[] args) {
